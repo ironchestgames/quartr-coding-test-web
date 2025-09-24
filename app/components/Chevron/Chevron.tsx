2fcac92638885a63106450eef1e99c8e4e0a62a0
Fredrik Järnbröst <github@ironchest.se>
2025-09-24 08:46:15 +0200

    Add Chevron component

---
+++ app/components/Chevron/Chevron.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+const Chevron = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none">
+    <path
+      stroke="#999"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="m9 18 6-6-6-6"
+    />
+  </svg>
+);
+
+export default Chevron;
